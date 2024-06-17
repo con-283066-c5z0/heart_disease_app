f64ae43163be0ee081ee18bfdaaf3975f377409f
con-283066-c5z0 <con-283066-c5z0@vossie.net>
2024-06-17 11:29:17 +0200

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -1,1 +1,12 @@
+#Name of table
+table_name = "heart_table"
 
+#Reopened the connection to the SQLite database
+connection = sqlite3.connect("heart.db")
+
+#Get data from db to df
+query = f"SELECT * FROM {table_name};"
+df = pd.read_sql(query, connection)
+
+#Closed the connection
+connection.close()
