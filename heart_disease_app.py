35e6c68827be21a48b71ce392264ac37b6b0de1a
con-283066-c5z0 <con-283066-c5z0@vossie.net>
2024-06-17 11:34:45 +0200

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -1,3 +1,10 @@
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+import pandas as pd
+import sqlite3
+
 #Name of table
 table_name = "heart_table"
 
@@ -10,3 +17,30 @@ df = pd.read_sql(query, connection)
 
 #Closed the connection
 connection.close()
+
+#Defined features and target
+X = df.drop(columns='target')
+y = df['target']
+
+#Identified numerical and categorical columns
+numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
+categorical_cols = X.select_dtypes(include=['object', 'int64']).columns.difference(numerical_cols)
+
+#Preprocessed for numerical data: imputer used for missing values and scaler for removing the mean scaling to unit varience.
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
+
+#Preprocessed for categorical data: impute missing values and one-hot encode
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+#Combine preprocessing steps
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
